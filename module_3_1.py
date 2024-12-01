16d7ae8654b58bd633c00f7a0dcf80ab508d555e
oddim <allayarovdim@gmail.com>
2024-12-02 00:03:52 +0500

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -1,30 +1,28 @@
 calls = 0
-while True:
-    string = input('Введите слово: ')
-    list_to_search = input('Введите список (каждый элемент через пробел): ').split()
-    if string == '':
-        break
+def count_calls():
+    global calls
+    calls = calls + 1
+    return calls
+def string_info(string):
+    global calls
+    count_calls()
+    string = (len(string), string.upper(), string.lower())
+    return string
+def is_contains (string, list_to_search):
+    global calls
+    count_calls()
+    for i in range(0, len(list_to_search)):
+        list_to_search[i] = list_to_search[i].lower()
+    if string.lower() in list_to_search:
+        print(True)
     else:
-        def count_calls():
-            global calls
-            calls = calls + 1
-            return calls
-        def string_info(string):
-            global calls
-            count_calls()
-            string = (len(string), string.upper(), string.lower())
-            return string
-        def is_contains (string, list_to_search):
-            global calls
-            count_calls()
-            for i in range(0, len(list_to_search)):
-                list_to_search[i] = list_to_search[i].lower()
-            if string.lower() in list_to_search:
-                print(True)
-            else:
-                print(False)
-            return string, list_to_search
+        print(False)
+print(string_info('Capybara'))
 
-    print(string_info(string))
-    is_contains(string, list_to_search)
-    print(calls)
+print(string_info('Armageddon'))
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+
+print(calls)
